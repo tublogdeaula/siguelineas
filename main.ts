a7ef27091640a8e706d51438f26199ba6ca51a0b
Aula MatemáTICa <52886636+tublogdeaula@users.noreply.github.com>
2023-01-03 00:23:17 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 225)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 225)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 225)
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 225)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            }
+        } else {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 225)
+                    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                }
+            }
+        }
+    }
+})
+basic.forever(function () {
+    basic.pause(100)
+    basic.showString("Blanca, Copito y Maqueen")
+    basic.pause(100)
+})
